c807c2b528de7adc25d79ab71fd4050a10f737f6
= <=>
2022-08-29 11:59:02 +0530

    Initial Commit from omkar

---
+++ src/com/github/empname/PrintEmpName.java
@@ -0,0 +1,11 @@
+package com.github.empname;
+
+public class PrintEmpName {
+
+	public static void main(String[] args) {
+
+		// HII I am OMKAR: You have to write Code to print your name: Use String concept
+
+	}
+
+}
